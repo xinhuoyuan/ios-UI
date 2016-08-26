9c68044c9a3c17abc1e1097ba4f6cce6e1768449
xinhuoyuan <apple@promote.cache-dns.local>
2016-08-26 14:16:03 +0800

    自定义登高的cell-storyboard
    自定义登高的cell-storyboard

---
+++ 自定义等高的cell-storyboard/自定义等高的cell-storyboard/JMTgsCell.h
@@ -0,0 +1,15 @@
+//
+//  JMTgsCell.h
+//  自定义等高的cell-storyboard
+//
+//  Created by apple on 16/8/26.
+//  Copyright © 2016年 XinHuoYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class JMTgs;
+
+@interface JMTgsCell : UITableViewCell
+@property (nonatomic,strong)JMTgs *tgs;
+@end
---
+++ 自定义等高的cell-storyboard/自定义等高的cell-storyboardTests/______cell_storyboardTests.m
@@ -0,0 +1,39 @@
+//
+//  ______cell_storyboardTests.m
+//  自定义等高的cell-storyboardTests
+//
+//  Created by apple on 16/8/26.
+//  Copyright © 2016年 XinHuoYuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ______cell_storyboardTests : XCTestCase
+
+@end
+
+@implementation ______cell_storyboardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
