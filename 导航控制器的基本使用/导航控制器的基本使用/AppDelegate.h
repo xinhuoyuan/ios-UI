2420a9e48095a5320b2ee2549542c620eeb8df59
xinhuoyuan <apple@promote.cache-dns.local>
2016-09-05 13:20:18 +0800

    导航控制器的基本使用
    导航控制器的基本使用

---
+++ 导航控制器的基本使用/导航控制器的基本使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  导航控制器的基本使用
+//
+//  Created by apple on 16/9/5.
+//  Copyright © 2016年 XinHuoYuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
